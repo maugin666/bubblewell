dc8d6ac8f482f48e862388ea866ca79aefafaefb
Levanova Evgeniya <maugin666@gmail.com>
2017-07-25 13:11:45 +0500

    commit#1

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var
+  gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  sass = require('gulp-sass')/*,
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  sourcemaps = require('gulp-sourcemaps'),
+  cssmin = require('gulp-minify-css'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify')*/;
+
+var path = {
+  src: {
+    html: 'src/',
+    js: 'src/javascripts/',
+    style: 'src/stylesheets/'
+  },
+  /*dev: {
+    html: 'dev/*.html',
+    js: [
+      'dev/javascripts/lib/jquery-3.2.1.js',
+      'dev/javascripts/lib/handlebars-v4.0.10.js',
+      'dev/javascripts/*.js'
+      ],
+    style: 'dev/stylesheets/base.scss'
+  },*/
+  watch: {
+    html: 'src/*.html',
+    js: 'src/javascripts/**/*.js',
+    style: 'src/stylesheets/base.scss'
+  }
+};
+
+/*gulp.task('html:build', function () {
+  gulp.src(path.dev.html)
+    .pipe(gulp.dest(path.prod.html));
+});*/
+
+gulp.task('style:build', function () {
+  gulp.src('src/stylesheets/*.scss')
+    //.pipe(sourcemaps.init())
+    .pipe(sass())
+    //.pipe(concat('style.css'))
+    //.pipe(postcss([autoprefixer()]))
+    //.pipe(cssmin())
+    //.pipe(sourcemaps.write())
+    .pipe(gulp.dest('src/stylesheets/'));
+});
+
+/*gulp.task('js:build', function () {
+  gulp.src(path.dev.js)
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.prod.js));
+});*/
+
+gulp.task('watch', function() {
+  //gulp.watch([path.watch.html], ['html:build']);
+  gulp.watch(['src/stylesheets/*.scss'], ['style:build']);
+  //gulp.watch([path.watch.js], ['js:build']);
+});
+
+gulp.task('build', ['html:build', 'style:build', 'js:build']);
